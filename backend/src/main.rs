32e53ab3feb41fd6ccb498912eddfaaa2b0df362
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-01-13 12:52:04 -0700

    create backend main and dockerfile

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,26 @@
-#[macro_use]
-extern crate rocket;
+use rocket::tokio::time::{sleep, Duration};
+use rocket::*;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, World!"
 }
 
+#[get("/jahsauce")]
+fn jahsauce() -> &'static str {
+    "Yippee!"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![jahsauce])
+        .mount("/", routes![delay])
 }

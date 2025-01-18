cd803033c15f7b050f905fe0e6c7c22c475f4557
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-01-18 12:24:33 -0700

    getting post from index route works!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,9 +5,29 @@ use rocket::tokio::time::{sleep, Duration};
 use rocket::*;
 
 #[get("/")]
-fn index() -> &'static str {
-    println!("hello");
-    "Hello, World!"
+fn index() -> String {
+    use portable_media::schema::post::dsl::*;
+
+    let pg_connection = &mut establish_connection();
+
+    let results = post
+        .limit(5)
+        .select(Post::as_select())
+        .load(pg_connection)
+        .expect("Error loading posts.");
+
+    let mut to_return = String::from("");
+
+    for p in results {
+        to_return.push_str(&p.post_id.to_string());
+        to_return.push_str("\n----------------\n");
+        to_return.push_str(match &p.description {
+            Some(d) => &d,
+            None => "No description",
+        });
+    }
+
+    format!("Posts:\n\n{}", to_return)
 }
 
 #[get("/jahsauce")]
@@ -23,28 +43,6 @@ async fn delay(seconds: u64) -> String {
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    use portable_media::schema::post::dsl::*;
-
-    let pg_connection = &mut establish_connection();
-
-    let results = post
-        .limit(5)
-        .select(Post::as_select())
-        .load(pg_connection)
-        .expect("Error loading posts.");
-
-    for p in results {
-        println!("{}", p.post_id);
-        println!("-----------");
-        println!(
-            "{}",
-            match p.description {
-                Some(d) => d,
-                None => "No description".to_string(),
-            }
-        );
-    }
-
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![jahsauce])

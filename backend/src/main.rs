6a9152e48afc644c8e4547626b731183de7c19ea
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-01-18 12:12:40 -0700

    selecting from posts table successfully!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,12 @@
+use diesel::prelude::*;
+use portable_media::establish_connection;
+use portable_media::models::Post;
 use rocket::tokio::time::{sleep, Duration};
 use rocket::*;
 
 #[get("/")]
 fn index() -> &'static str {
+    println!("hello");
     "Hello, World!"
 }
 
@@ -19,6 +23,28 @@ async fn delay(seconds: u64) -> String {
 
 #[launch]
 fn rocket() -> Rocket<Build> {
+    use portable_media::schema::post::dsl::*;
+
+    let pg_connection = &mut establish_connection();
+
+    let results = post
+        .limit(5)
+        .select(Post::as_select())
+        .load(pg_connection)
+        .expect("Error loading posts.");
+
+    for p in results {
+        println!("{}", p.post_id);
+        println!("-----------");
+        println!(
+            "{}",
+            match p.description {
+                Some(d) => d,
+                None => "No description".to_string(),
+            }
+        );
+    }
+
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![jahsauce])
--- backend/src/models.rs
+++ backend/src/models.rs
@@ -4,17 +4,20 @@ use diesel::{data_types::PgTimestamp, prelude::*};
 #[diesel(table_name = crate::schema::photo)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Photo {
-    photo_id: i32,
-    post_id: i32,
-    description: Option<String>,
-    photographer: Option<String>,
-    photo_path: String,
-    time_taken: PgTimestamp,
+    pub photo_id: i32,
+    pub post_id: i32,
+    pub description: Option<String>,
+    pub photographer: Option<String>,
+    pub photo_path: String,
+    pub time_taken: PgTimestamp,
 }
 
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::post)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Post {
-    post_id: i32,
-    description: Option<String>,
-    like_count: Option<i32>,
-    song: Option<String>,
+    pub post_id: i32,
+    pub description: Option<String>,
+    pub like_count: Option<i32>,
+    pub song: Option<String>,
 }

68d1e28aa4e35cd206cb90672bf3cc4ee068deac
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-01-19 17:31:57 -0700

    creating post works! as does getting new posts. yippee

---
+++ backend/application/src/post/create.rs
@@ -0,0 +1,29 @@
+use diesel::prelude::*;
+use domain::models::Post;
+use infrastructure::establish_connection;
+use rocket::response::status::Created;
+use rocket::serde::json::Json;
+use shared::response_models::{Response, ResponseBody};
+
+pub fn create_post(new_post: Json<Post>) -> Created<String> {
+    use domain::schema::posts;
+
+    let new_post = new_post.into_inner();
+
+    match diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result::<Post>(&mut establish_connection())
+    {
+        Ok(post) => {
+            let response = Response {
+                body: ResponseBody::Post(post),
+            };
+            Created::new("").tagged_body(serde_json::to_string(&response).unwrap())
+        }
+        Err(err) => match err {
+            _ => {
+                panic!("Database error - {}", err);
+            }
+        },
+    }
+}

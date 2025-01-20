43b587c341589722ac8403237b55da0a792e6c3a
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-01-19 21:24:05 -0700

    Finally got DateTime working!

--- backend/domain/src/models.rs
+++ backend/domain/src/models.rs
@@ -1,31 +1,47 @@
 use chrono::{DateTime, Utc};
-use diesel::prelude::*;
+use diesel::{deserialize::FromSqlRow, pg::Pg, prelude::*, sql_types::Timestamptz};
 use rocket::{
     form::{self, FromForm, FromFormField, ValueField},
     serde::{Deserialize, Serialize},
 };
 
+// Had to create DateTimeUtcForm so that I can create impls for DateTime as you can't impl for types
+// defined in external crates
 #[derive(Queryable, QueryableByName, Selectable, Ord, Eq, PartialEq, PartialOrd, Debug)]
 #[diesel(table_name = crate::schema::photos)]
 pub struct DateTimeUtcForm {
     pub time_taken: DateTime<Utc>,
 }
 
+// getter function
 impl DateTimeUtcForm {
     pub fn inner(&self) -> &DateTime<Utc> {
         &self.time_taken
     }
 }
 
+// Required for rocket FromForm macro
 impl<'r> FromFormField<'r> for DateTimeUtcForm {
     fn from_value(field: ValueField<'r>) -> form::Result<'r, Self> {
-        println!("{}", field.value);
         match DateTime::parse_from_str(field.value, "%Y-%m-%d %H:%M") {
             Ok(date) => Ok(DateTimeUtcForm {
                 time_taken: date.into(),
             }),
-            Err(e) => panic!("{}", e),
+            Err(e) => panic!("Error parsing DateTime - {}", e),
         }
+    }
+}
+
+// Required for diesel FromSqlRow trait
+impl FromSqlRow<Timestamptz, Pg> for DateTimeUtcForm {
+    fn build_from_row<'a>(
+        row: &impl diesel::row::Row<'a, Pg>,
+    ) -> diesel::deserialize::Result<Self> {
+        // Convert from the SQL `Timestamptz` to DateTime<Utc>
+        let timestamp: DateTime<Utc> = FromSqlRow::<Timestamptz, Pg>::build_from_row(row)?;
+        Ok(DateTimeUtcForm {
+            time_taken: timestamp,
+        })
     }
 }
 
@@ -64,6 +80,7 @@ pub struct Post {
     pub song: Option<String>,
 }
 
+// Required for serializing/deserializing DateTimeUtcForm
 mod date_format {
     use super::DateTimeUtcForm;
     use chrono::{DateTime, NaiveDateTime, Utc};

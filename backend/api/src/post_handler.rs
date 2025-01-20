5d15ba6b7fd5a732e4741aff60821759205f85e0
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-01-19 19:59:24 -0700

    delete functionality works

--- backend/api/src/post_handler.rs
+++ backend/api/src/post_handler.rs
@@ -1,4 +1,4 @@
-use application::post::{create, read};
+use application::post::{create, delete, read};
 use domain::models::Post;
 use rocket::response::status::{Created, NotFound};
 use rocket::serde::json::Json;
@@ -28,4 +28,14 @@ pub fn list_post_handler(post_id: i32) -> Result<String, NotFound<String>> {
 #[post("/new_post", format = "application/json", data = "<post>")]
 pub fn create_post_handler(post: Json<Post>) -> Created<String> {
     create::create_post(post)
+}
+
+#[get("/delete/<post_id>")]
+pub fn delete_post_handler(post_id: i32) -> Result<String, NotFound<String>> {
+    let posts = delete::delete_post(post_id)?;
+    let response = Response {
+        body: ResponseBody::Posts(posts),
+    };
+
+    Ok(serde_json::to_string(&response).unwrap())
 }
--- backend/application/src/post/delete.rs
+++ backend/application/src/post/delete.rs
@@ -1,16 +1,16 @@
+use diesel::prelude::*;
 use domain::models::Post;
 use infrastructure::establish_connection;
 use rocket::response::status::NotFound;
 use shared::response_models::{Response, ResponseBody};
 
-pub fn delete_post(post_id: i32) -> Result<Vec<Post>, NotFound<String>> {
-    use domain::schema::posts;
+pub fn delete_post(id: i32) -> Result<Vec<Post>, NotFound<String>> {
     use domain::schema::posts::dsl::*;
 
     let response: Response;
 
     let num_deleted =
-        match diesel::delete(posts.filter(id.eq(post_id))).execute(&mut establish_connection()) {
+        match diesel::delete(posts.filter(post_id.eq(id))).execute(&mut establish_connection()) {
             Ok(count) => count,
             Err(err) => match err {
                 diesel::result::Error::NotFound => {
@@ -20,7 +20,7 @@ pub fn delete_post(post_id: i32) -> Result<Vec<Post>, NotFound<String>> {
                             post_id, err
                         )),
                     };
-                    Err(NotFound(serde_json::to_string(&response).unwrap()))
+                    Err(NotFound(serde_json::to_string(&response).unwrap()))?
                 }
                 _ => {
                     panic!("Database error - {}", err);
@@ -28,5 +28,25 @@ pub fn delete_post(post_id: i32) -> Result<Vec<Post>, NotFound<String>> {
             },
         };
 
-    todo!()
+    if num_deleted > 0 {
+        match posts
+            .select(posts::all_columns())
+            .load::<Post>(&mut establish_connection())
+        {
+            Ok(mut posts_) => {
+                posts_.sort();
+                return Ok(posts_);
+            }
+            Err(err) => match err {
+                _ => {
+                    panic!("Database error - {}", err);
+                }
+            },
+        }
+    } else {
+        response = Response {
+            body: ResponseBody::Message(format!("Error - no post with id {}", id)),
+        };
+        Err(NotFound(serde_json::to_string(&response).unwrap()))?
+    }
 }

979372033de89001e9e78d08cc6923d5f346b370
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-02-08 16:00:05 -0700

    Fixing sql to match the new backend structures

---
+++ backend/infrastructure/migrations/2025-02-08-225602_create_photos_posts/up.sql
@@ -0,0 +1,26 @@
+CREATE SEQUENCE IF NOT EXISTS seq_post_id
+AS INTEGER
+INCREMENT BY 2
+START WITH 2;
+
+CREATE TABLE posts (
+  post_id INTEGER PRIMARY KEY UNIQUE NOT NULL DEFAULT nextval('seq_post_id'),
+  description VARCHAR(2048),
+  like_count INTEGER,
+  song VARCHAR(128),
+  time timestamptz NOT NULL DEFAULT (now() at time zone 'utc')
+);
+
+CREATE SEQUENCE IF NOT EXISTS seq_photo_id
+AS INTEGER
+INCREMENT BY 2
+START WITH 1;
+
+CREATE TABLE photos (
+  photo_id INTEGER PRIMARY KEY UNIQUE NOT NULL DEFAULT nextval('seq_post_id'),
+  post_id INTEGER NOT NULL,
+  description VARCHAR(128),
+  photographer VARCHAR(128),
+  photo_path VARCHAR(128) NOT NULL,
+  CONSTRAINT fk_post_id FOREIGN KEY (post_id) REFERENCES "posts" (post_id)
+);
